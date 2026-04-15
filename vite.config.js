78a3745ce65bcd4f36b273548ef0c5a5d272d684
Duma4rman <nickitolabruh@gmail.com>
2026-04-15 18:45:19 +0300

    Fix: Set base path in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Базовый путь для GitHub Pages
-const BASE_URL = process.env.BASE_URL || '/'
-
 export default defineConfig({
   plugins: [react()],
-  base: BASE_URL,
+  base: '/nogti-world/',
 })
